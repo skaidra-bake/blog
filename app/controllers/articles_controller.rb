422021d8c6ea707a6faa8e2ea815df9d4781f296
Skaidra Bake <skaidrapat@gmail.com>
2020-11-04 18:51:33 +0000

    Added Index Listing of Articles, and Add Edit/Show Links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -32,7 +32,11 @@ class ArticlesController < ApplicationController
             render "edit"
         end
     end
- 
+
+    def index
+        @articles = Article.all
+    end
+
     private
 
     def article_params
