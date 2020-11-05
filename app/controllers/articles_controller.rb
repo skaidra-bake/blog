c26bc6914bb07acd87040ad2ccb06df0046a2816
Skaidra Bake <skaidrapat@gmail.com>
2020-11-05 15:02:38 +0000

    articles controller refactored (applying DRY principle of development)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:edit, :update, :show, :destroy]
     def new
         @article = Article.new
     end
@@ -15,15 +16,12 @@ class ArticlesController < ApplicationController
     end
 
     def show
-        @article = Article.find(params[:id])
     end
 
     def edit
-        @article = Article.find(params[:id])
     end
 
     def update
-        @article = Article.find(params[:id])
         if @article.update(article_params)
             flash[:notice] = "Article was updated"
             redirect_to article_path(@article)
@@ -38,7 +36,6 @@ class ArticlesController < ApplicationController
     end
 
     def destroy
-        @article = Article.find(params[:id])
         @article.destroy
         flash[:notice] = "Article was deleted"
         redirect_to articles_path
@@ -48,6 +45,10 @@ class ArticlesController < ApplicationController
 
     def article_params
         params.require(:article).permit(:title, :description)
+    end
+
+    def set_article
+        @article = Article.find(params[:id])
     end
 
 end
