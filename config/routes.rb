afbd421b4dbac9a71c5fb7c582b097cb95a17512
Skaidra Bake <skaidrapat@gmail.com>
2020-11-08 19:29:32 +0000

    changed navbar (correct version and dark colour scheme)

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   root 'pages#index'
-  resources :articles
+  resources :articles #, path: 'cars'
   get 'about', to: 'pages#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
