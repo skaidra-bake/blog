0f55fe6cfc5005749bedfebdcfc77e6d216e445c
skaidra-bake <skaidrapat@gmail.com>
2020-11-11 02:00:09 +0000

    signed up users, added a secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
     has_many :articles
+    has_secure_password
     validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 3, maximum: 25}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\z/i
     validates :email, presence: true, uniqueness: {case_sensitive: false}, length: {maximum: 105}, format: {with: VALID_EMAIL_REGEX}
