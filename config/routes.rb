30b7daac3bfc16eb90425903c4542d9c35bb981d
skaidra-bake <skaidrapat@gmail.com>
2020-11-12 00:48:13 +0000

    added login and logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   root 'pages#index'
   get 'about', to: 'pages#about'
   get 'signup', to: 'users#new'
-
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
